53bd5d5da0f64e9bfc3abc7058db139f2e014162
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2019-10-15 13:33:50 -0400

    travis file, example, requirements, class object and recursion

---
+++ example.py
@@ -0,0 +1,10 @@
+from slayer import SlayerTool
+
+st = SlayerTool()
+
+st.create_graph()
+
+st.create_graph('Vannaka', 'jimbo jango')
+
+st.create_graph(4, 'jimbo jango')
+# st.create_graph(5)
--- slayer.py
+++ slayer.py
@@ -8,205 +8,235 @@ from hiscores import Hiscores
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 plt.rcdefaults()
-data_file = "./slayer.json"
-increment_file = "./counter.json"
-account = 0
+author = 'James Cerniglia'
 
-# Counter File for incrementation
-with open(increment_file) as json_file:
-    count = json.load(json_file)
-count.update({'counter': count['counter']+1})
-
-with open(increment_file, 'w') as outfile:
-    json.dump(count, outfile)
-
-# Slayer file
-with open(data_file) as json_file:
-    slayer_data = json.load(json_file)
-
-# Masters
-masters = {0: "Turael",
-           1: "Mazchna",
-           2: "Vannaka",
-           3: "Chaelder",
-           4: "Duradel",
-           5: "Nieve",
-           6: "Krystilia",
-           7: "Konar quo Maten"
-           }
-
-# Master assignment data initialization
-vannaka_assignments = slayer_data['Vannaka']['Assignments']
-nieve_assignments = slayer_data['Nieve']['Assignments']
-duradel_assignments = slayer_data['Duradel']['Assignments']
-krystilia = slayer_data['Krystilia']
-chae = slayer_data['Chaelder']
-konar = slayer_data['Konar quo Maten']
-turael = slayer_data['Turael']
 # Runtime Execution
 # slayer.py 'slayer_master' 'sample_size'
 
 
-def get_doable_assignments(input_slayer_level):
-    doable_tasks = {}
-    for m in slayer_data:
-        # m = master
-        doable_tasks[m] = {}
-        doable_tasks[m]['Assignments'] = {}
-        sum1 = 0
-        for t in slayer_data[m]['Assignments'].items():
-            # t= task
-            addTask = True
-            if 'UnlockRequirements' in slayer_data[m]['Assignments'][t[0]]:
-                if 'Slayer' in slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']:
-                    if account.skills['slayer'].level >= slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer']:
-                        print('higher : ', account.skills['slayer'].level, ' vs ',
-                              slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer'])
-                        addTask = True
-                    else:
-                        addTask = False
-                if 'Combat' in slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']:
-                    if account.combat >= slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer']:
-                        addTask = True
-                    else:
-                        addTask = False
-            else:
-                print('no requirements for task:', t[0])
+class SlayerTool(object):
+    data_file = "./slayer.json"
+    increment_file = "./counter.json"
+    account = 0
+    combat_level = 0
 
-            if addTask:
-                doable_tasks[m]['Assignments'].update({t[0]: t[1]})
-                sum1 = sum1+slayer_data[m]['Assignments'][t[0]]['Weight']
+    with open(data_file) as json_file:
+        slayer_data = json.load(json_file)
 
-        doable_tasks[m]['TotalWeight'] = sum1
+    def __init__(self, *args):
+        if len(args) > 1:
+            self.account = Hiscores(args[0])
+            self.combat_level = self.get_cb_lvl(self.account)
+        else:
+            self.account = Hiscores('Lynx Titan')
+            self.combat_level = self.get_cb_lvl(self.account)
+        # Counter File for incrementation
+        with open(self.increment_file) as json_file:
+            self.count = json.load(json_file)
+        self.count.update({'counter': self.count['counter']+1})
 
-    with open('doable.json', 'w') as outfile:
-        json.dump(doable_tasks, outfile)
+        with open(self.increment_file, 'w') as outfile:
+            json.dump(self.count, outfile)
 
+    # Masters
+    masters = {0: "Turael", 1: "Mazchna", 2: "Vannaka", 3: "Chaelder",
+               4: "Duradel", 5: "Nieve", 6: "Krystilia", 7: "Konar quo Maten"}
 
-def evaluate_assignment(dict_x):
-    for i in dict_x['Assignments']:
-        print('==', i, '==')
-        if 'UnlockRequirements' in dict_x['Assignments'][i]:
-            for i in dict_x['Assignments'][i]['UnlockRequirements']:
+    # Master assignment data initialization
+    vannaka_assignments = slayer_data['Vannaka']
+    nieve_assignments = slayer_data['Nieve']
+    duradel_assignments = slayer_data['Duradel']
+    krystilia = slayer_data['Krystilia']
+    chaelder = slayer_data['Chaelder']
+    konar = slayer_data['Konar quo Maten']
+    turael = slayer_data['Turael']
+
+    def set_account(self, x):
+        '''
+        Set Acount for object reference, only needed to query once instead of a million times
+        '''
+        self.account = Hiscores(x)
+
+    def reset_counter(self):
+        '''
+        Reset counter for file outputs
+        '''
+        self.count = 0
+        with open(self.increment_file, 'w') as outfile:
+            json.dump(self.count, outfile)
+
+    def get_doable_assignments(self, input_slayer_level):
+        doable_tasks = {}
+        for m in slayer_data:
+            # m = master
+            doable_tasks[m] = {}
+            doable_tasks[m]['Assignments'] = {}
+            sum1 = 0
+            for t in slayer_data[m]['Assignments'].items():
+                # t= task
+                addTask = True
+                if 'UnlockRequirements' in slayer_data[m]['Assignments'][t[0]]:
+                    if 'Slayer' in slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']:
+                        if account.skills['slayer'].level >= slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer']:
+                            print('higher : ', account.skills['slayer'].level, ' vs ',
+                                  slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer'])
+                            addTask = True
+                        else:
+                            addTask = False
+                    if 'Combat' in slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']:
+                        if account.combat >= slayer_data[m]['Assignments'][t[0]]['UnlockRequirements']['Slayer']:
+                            addTask = True
+                        else:
+                            addTask = False
+                else:
+                    print('no requirements for task:', t[0])
+
+                if addTask:
+                    doable_tasks[m]['Assignments'].update({t[0]: t[1]})
+                    sum1 = sum1+slayer_data[m]['Assignments'][t[0]]['Weight']
+
+            doable_tasks[m]['TotalWeight'] = sum1
+
+        with open('doable.json', 'w') as outfile:
+            json.dump(doable_tasks, outfile)
+
+    def evaluate_assignment(self, dict_x, assignment):
+
+        if 'UnlockRequirements' in dict_x['Assignments'][assignment]:
+            print(dict_x['Assignments'][assignment]['UnlockRequirements'])
+            for i in dict_x['Assignments'][assignment]['UnlockRequirements']:
                 if i == 'Slayer':
-                    var = 0
+                    if self.account.skills['slayer'].level >= dict_x['Assignments'][assignment]['UnlockRequirements']['Slayer']:
+                        doable = True
+                    else:
+                        doable = False
                 if i == 'Combat':
-                    var = 0
+                    if self.combat_level >= dict_x['Assignments'][assignment]['UnlockRequirements']['Combat']:
+                        doable = True
+                    else:
+                        doable = False
+                if i != 'or' or 'Combat' or 'Quests' or 'partialQuests':
+                    # any skill but those
+                    todo=0
                 if i == 'Quests':
-                    var = 0
+                    # TODO
+                    have_not_implemented_quest_checking=0
                 if i == 'partialQuests':
-                    var = 0
+                     # TODO
+                    have_not_implemented_quest_checking=0
         else:  # Krystilia else statement, none of her tasks have requirements.
             var = 0
 
+    def create_graph(self, *args):
+        '''
+        Creates graph based on input variables
+        args[0] = Slayer master ('0' or 'Vannaka')          default: random
+        args[1] = sample size                               default: 1000
+        args[2] = username or level ('jimbo jango' or '65') default: 99
+        '''
 
-def test(*args):
+        master_name = self.masters.get(random.randint(
+            0, len(self.masters)-1), "Please either use the name of the master or an integer from the dictionary"+str(self.masters))
+        dict_x = self.slayer_data[master_name]
+        sample_size = 1000
+        if len(args) > 0:
+            # can either use "0" for Tureal or 'Vannaka' for respected slayer master
 
-    args = args[0]
-    random_master = random.randint(0, 7)
-    master_name = masters.get(random_master, "Indoable Number")
-    dict_x = slayer_data[master_name]
-    sample_size = 10000
+            if isinstance(args[0], str):
+                master_name = args[0]
+                dict_x = self.slayer_data[args[0]]
 
-    if len(args) > 0:
-        master_name = masters.get(int(args[0]), "Indoable Number")
-        dict_x = slayer_data[master_name]
+            elif isinstance(args[0], int):
+                master_name = self.masters.get(int(
+                    args[0]), "Please either use the name of the master or an integer from the dictionary : "+str(self.masters))
+                dict_x = self.slayer_data[master_name]
 
-        if len(args) > 1:
-            sample_size = int(args[1])
+            # Default sample size is 1000 but they can set it with one more number
+            if len(args) > 1:
+                if isinstance(args[2], int):
+                    sample_size = int(args[1])
 
-    # Creating our data based on our sample size
+        # Creating our data based on our sample size
 
-    # Paths for saving data
-    figure_path = './Images/'+master_name+str(count['counter'])+'.png'
-    data_path = './Data/'+master_name+str(count['counter'])+'.json'
+        # Paths for saving data
+        figure_path = './Images/'+master_name+str(self.count['counter'])+'.png'
+        data_path = './Data/'+master_name+str(self.count['counter'])+'.json'
 
-    # Get Total Weight from master
-    total_slayer_weight = dict_x['TotalWeight']
+        # Get Total Weight from master
+        total_slayer_weight = dict_x['TotalWeight']
 
-    assign_names = []
-    weight_array = []
-    assign_counter_dict = {}
-    sum1 = 0
-    for i in dict_x['Assignments']:
-        assign_names.append(i)
-        # print(i, " : ", dict_x['Assignments'][i]['Weight']) # prints the task with the weight.
+        assign_names = []
+        weight_array = []
+        assign_counter_dict = {}
+        sum1 = 0
+        for i in dict_x['Assignments']:
 
-        weight_array.append((dict_x['Assignments'][i]['Weight'] /
-                             total_slayer_weight))
-        assign_counter_dict.update({i: 0})
-        # print(sum(weight_array))
-        sum1 = sum1+dict_x['Assignments'][i]['Weight']
+            x = self.evaluate_assignment(dict_x, i)
+            print(x)
+            assign_names.append(i)
+            # print(i, " : ", dict_x['Assignments'][i]['Weight']) # prints the task with the weight.
 
-    # print(sum1)# prints the sum of all the weights, should always be equal to the masters total weight.
+            weight_array.append((dict_x['Assignments'][i]['Weight'] /
+                                 total_slayer_weight))
+            assign_counter_dict.update({i: 0})
+            # print(sum(weight_array))
+            sum1 = sum1+dict_x['Assignments'][i]['Weight']
 
-    for i in range(0, sample_size):
-        rnd = choice(assign_names, p=weight_array)
-        assign_counter_dict.update({rnd: assign_counter_dict[rnd]+1})
+        # print(sum1)# prints the sum of all the weights, should always be equal to the masters total weight.
 
-    # # Initializing graph arrays with x and y columns
-    performance = []
-    objects = []
+        for i in range(0, sample_size):
+            rnd = choice(assign_names, p=weight_array)
+            assign_counter_dict.update({rnd: assign_counter_dict[rnd]+1})
 
-    for i in assign_counter_dict:
-        # print(i, assign_counter_dict[i]) #prints the assignment ant how many times it was chosen while adding everything to 2 data arrays
-        performance.append(assign_counter_dict[i])
-        objects.append(i)
+        # # Initializing graph arrays with x and y columns
+        performance = []
+        objects = []
 
-    # Creating actual figure
-    y_pos = np.arange(len(assign_counter_dict))
-    plt.bar(objects, performance, align='center', alpha=0.5, color='red')
-    plt.xticks(y_pos, objects, rotation=90, fontsize=6)
-    plt.xlabel('Monster')
-    plt.title(master_name)
-    plt.show()
-    plt.savefig(figure_path)
+        for i in assign_counter_dict:
+            # print(i, assign_counter_dict[i]) #prints the assignment ant how many times it was chosen while adding everything to 2 data arrays
+            performance.append(assign_counter_dict[i])
+            objects.append(i)
 
-    # Saves data to a json if the user wants to see the data and use it for something else.
-    with open(data_path, 'w') as outfile:
-        json.dump(assign_counter_dict, outfile)
+        # Creating actual figure
+        y_pos = np.arange(len(assign_counter_dict))
+        plt.bar(objects, performance, align='center', alpha=0.5, color='red')
+        plt.xticks(y_pos, objects, rotation=90, fontsize=6)
+        plt.xlabel('Monster')
+        plt.title(master_name)
+        plt.show()
+        plt.savefig(figure_path)
 
+        # Saves data to a json if the user wants to see the data and use it for something else.
+        with open(data_path, 'w') as outfile:
+            json.dump(assign_counter_dict, outfile)
 
-# test(sys.argv[1:])
+    def get_cb_lvl(self, acc):
+        x = [0.325*(acc.skills['attack'].level + acc.skills['strength'].level), 0.325 *
+             (int(3 * acc.skills['ranged'].level / 2)), 0.325*(int(3 * acc.skills['magic'].level / 2))]
+        x.sort()
 
+        return int(0.25 * (acc.skills['defence'].level +
+                           acc.skills['hitpoints'].level + (acc.skills['prayer'].level/2)) + x[-1])
 
-def get_cb_lvl(acc):
-    x = [0.325*(acc.skills['attack'].level + acc.skills['strength'].level), 0.325 *
-         (int(3 * acc.skills['ranged'].level / 2)), 0.325*(int(3 * acc.skills['magic'].level / 2))]
-    x.sort()
+    def get_help(self):
+        print(Fore.CYAN+'    == Current List of implemented commands ==')
+        print(Fore.BLUE + '\"-doable\"'+Fore.GREEN +
+              '     - Gets all the doable tasks at your current slayer level')
+        print(Fore.BLUE + '\"-g\"'+Fore.GREEN +
+              '          - Generates graphs based on tasks you\'re available to get')
+        print(Fore.BLUE + '\"-g -nolimit\"'+Fore.GREEN +
+              ' - Generates graphs at level 99 slayer all quests unlocked')
+        print(Fore.BLUE + '\"-doable\"'+Fore.GREEN +
+              '     - Gets all the doable tasks at your current slayer level')
 
-    return int(0.25 * (acc.skills['defence'].level +
-                       acc.skills['hitpoints'].level + (acc.skills['prayer'].level/2)) + x[-1])
-
-
-def get_help():
-    print(Fore.CYAN+'    == Current List of implemented commands ==')
-    print(Fore.BLUE + '\"-doable\"'+Fore.GREEN +
-          '     - Gets all the doable tasks at your current slayer level')
-    print(Fore.BLUE + '\"-g\"'+Fore.GREEN +
-          '          - Generates graphs based on tasks you\'re available to get')
-    print(Fore.BLUE + '\"-g -nolimit\"'+Fore.GREEN +
-          ' - Generates graphs at level 99 slayer all quests unlocked')
-    print(Fore.BLUE + '\"-doable\"'+Fore.GREEN +
-          '     - Gets all the doable tasks at your current slayer level')
-
-
-def start(arr):
-    if len(arr) >= 1:
-        command = arr[0]
-        if command == '-cmd':
-            get_help()
-        else:
-            ACCOUNT = arr[0].replace('_', ' ')
-            global account
-            account = Hiscores(ACCOUNT)
-            account.combat = get_cb_lvl(account)
-        
-
-
-if len(sys.argv) > 1:
-    start(sys.argv[1:])
-else:
-    print(Fore.GREEN+'No command line arguments given, try:')
-    print('py slayer.py '+Fore.BLUE+'\"user_name\" (use _ for spaces) '+Fore.RED+'\"command\" ' +
-          Fore.WHITE+'- '+Fore.YELLOW+'Need help? do  py slayer.py -cmd')
+    def __str__(self):
+        global author
+        stringify = ' == Slayer Tool Developed by '+author+' ==\n \
+                    Current methods inside of the Slayer Tool'
+        return super().__str__()
+    if len(sys.argv) > 1:
+        start(sys.argv[1:])
+    else:
+        print(Fore.GREEN+'No command line arguments given, try:')
+        print('py slayer.py '+Fore.BLUE+'\"user_name\" (use _ for spaces) '+Fore.RED+'\"command\" ' +
+              Fore.WHITE+'- '+Fore.YELLOW+'Need help? do  py slayer.py -cmd')
