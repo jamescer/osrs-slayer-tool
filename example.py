80bbf7e4ce17a3b5e721c3df7eaf6530644340a1
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2019-10-16 11:31:54 -0400

    hotfix

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ tr = SlayerTool(username='not poop')
 
 
 tr.get_doable_assignments()
-# tr.set_account('not poop')
-# tr.reset_counter()
+tr.set_account('not poop')
+tr.reset_counter()
 
-# tr.create_graph(master_name=0,sample_size=1000)
+tr.create_graph(master_name=0,sample_size=1000)
